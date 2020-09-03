b022713ddcb3473836936cf8d97ce694aa0e05c9
Apeksha <apeksha.2018562@iit.ac.lk>
2020-09-03 22:03:18 +0530

    new updates

--- src/main/java/com/example/RecipeApplication/repository/RecipesRepository.java
+++ src/main/java/com/example/RecipeApplication/repository/RecipesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
-public interface RecipesRepository extends CrudRepository<Recipe, Long> {
+public interface RecipesRepository extends CrudRepository<Recipe, Integer> {
 }
